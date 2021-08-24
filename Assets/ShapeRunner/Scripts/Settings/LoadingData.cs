a3a02d1eb836fed2842dc8b1289edabe06b66662
Fedor Romanovskii <fedorromanovskii@yandex.ru>
2021-08-24 22:48:48 +0300

    -add (loading data for transfer data from menu to game scene)

---
+++ Assets/ShapeRunner/Scripts/Settings/LoadingData.cs
@@ -0,0 +1,8 @@
+using ShapeRunner.Character;
+using ShapeRunner.World;
+
+public static class LoadingData
+{
+    public static CharacterConfig CharacterConfig;
+    public static WorldConfig WorldConfig;
+}
