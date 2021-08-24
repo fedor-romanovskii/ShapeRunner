2c97e279f2eef83e0b30d2b17bf45ce5df10dfeb
Fedor Romanovskii <fedorromanovskii@yandex.ru>
2021-08-24 23:26:13 +0300

    -update (prepare main menu for game starting)

--- Assets/ShapeRunner/Scripts/UI/MainMenu.cs
+++ Assets/ShapeRunner/Scripts/UI/MainMenu.cs
@@ -1,12 +1,20 @@
+using ShapeRunner.Settings;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace ShapeRunner.UI
 {
     public class MainMenu : MonoBehaviour
     {
+        private const string GameSceneName = "Game";
+
         [SerializeField]
         private Button _play;
+        [SerializeField]
+        private WorldChoosePanel _worldChoosePanel;
+        [SerializeField]
+        private CharacterChoosePanel _characterChoosePanel;
 
         private void OnEnable()
         {
@@ -21,6 +29,14 @@ namespace ShapeRunner.UI
         private void Play()
         {
             Debug.Log("MainMenu : Play");
+            SetupLoadingData();
+            SceneManager.LoadScene(GameSceneName);
+        }
+
+        private void SetupLoadingData()
+        {
+            LoadingData.CharacterConfig = _characterChoosePanel.Config;
+            LoadingData.WorldConfig = _worldChoosePanel.Config;
         }
     }
 }
