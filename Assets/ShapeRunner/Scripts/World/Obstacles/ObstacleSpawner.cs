c01e4a07104cce27bbbabbdfa51c7c5e0373a3c0
Fedor Romanovskii <fedorromanovskii@yandex.ru>
2021-08-28 13:30:04 +0300

    -add (obstacles prefabs) -update (obstacle spawner pool initialization, world configs obstacle prefabs reference)

--- Assets/ShapeRunner/Scripts/World/Obstacles/ObstacleSpawner.cs
+++ Assets/ShapeRunner/Scripts/World/Obstacles/ObstacleSpawner.cs
@@ -5,10 +5,23 @@ namespace ShapeRunner.World
     public class ObstacleSpawner : MonoBehaviour
     {
         private Obstacle[] _obstacles;
+        private ObjectPool<Obstacle>[] _pools;
 
         public void SetupPrefabs(Obstacle[] obstacles)
         {
             _obstacles = obstacles;
+            InitializePools();
+        }
+
+        private void InitializePools()
+        {
+            _pools = new ObjectPool<Obstacle>[_obstacles.Length];
+
+            for (int i = 0; i < _pools.Length; i++)
+            {
+                var obstacle = _obstacles[i];
+                _pools[i] = new ObjectPool<Obstacle>(obstacle, 3, transform);
+            }
         }
     }
 }
